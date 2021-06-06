a88e6016cb4803180665d7e5166fe2d07f924559
Jean Olele <>
2021-06-06 20:04:42 +0000

    removed brckets

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,12 +10,12 @@ let question =  " 1) : Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = [];
 
-let questions = [" : Who was the first American woman in space? " ,
- ": True or false: 5 kilometer == 5000 meters? ",
+let questions = [" 1) : Who was the first American woman in space? " ,
+ " 2) : True or false: 5 kilometer == 5000 meters?" ,
 
-  "  :(5 + 3)/2 * 10 = ?" , 
-  "  Given the array [8, 'Orbit', 'Trajectory', 45] , what entry is at index 2?" , 
-  "  : What is the minimum crew size for the ISS?"
+  " 3) :(5 + 3)/2 * 10 = ?" , 
+  ' 4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?' , 
+  " 5) : What is the minimum crew size for the ISS?"
   ];
 
 let correctAnswers = ["Sally Ride" ,
@@ -66,11 +66,11 @@ function gradeQuiz(candidateAnswers) {
         if(candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
         numberOfCorrectAnswer++  ;
 }
-    grade = numberOfCorrectAnswer / 5 * 100 ;
+    grade = (numberOfCorrectAnswer / 5 * 100) ;
  }
 
 
- console.log(` " >>> Overall Grade:" ${grade}% (${numberOfCorrectAnswer} "of 5 responses correct) <<< " ` );
+ console.log(` " >>> Overall Grade: " ${grade}% (${numberOfCorrectAnswer} "of 5 responses correct) <<< " ` );
 
 
 if (grade < 80) {
