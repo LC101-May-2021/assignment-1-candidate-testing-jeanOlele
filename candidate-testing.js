6c36b424c6fb61eba38dfcc0617ce8de96b7bde1
Jean Olele <>
2021-06-06 21:32:02 +0000

    questions number removed

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,20 +6,20 @@ const input = require('readline-sync');
 
 let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question =  "1) : Who was the first American woman in space? ";
+let question =  " Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = [];
 
 let questions = [
-    "1) : Who was the first American woman in space?" ,
+    " Who was the first American woman in space? " ,
 
-    "2) : True or false: 5 kilometer == 5000 meters?" ,
+    " True or false: 5 kilometer == 5000 meters? " ,
 
-    "3) :(5 + 3)/2 * 10 = ?" ,
+    " (5 + 3)/2 * 10 = ? " ,
 
-    '4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at   index 2?' , 
+    ' Given the array [8, "Orbit", "Trajectory", 45], what entry is at   index 2? ' , 
     
-    "5) : What is the minimum crew size for the ISS?"
+    " What is the minimum crew size for the ISS? "
   ];
 
 let correctAnswers = [
